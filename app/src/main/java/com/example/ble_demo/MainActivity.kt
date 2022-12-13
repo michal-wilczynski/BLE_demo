a301e1ac78fef66641e71e2e61349eb6ff638461
Michal Wilczynski <miy@spyro-soft.com>
2022-12-13 01:13:07 +0100

    stopscan deleted yet

--- app/src/main/java/com/example/ble_demo/MainActivity.kt
+++ app/src/main/java/com/example/ble_demo/MainActivity.kt
@@ -38,21 +38,11 @@ class MainActivity : AppCompatActivity() {
         .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
         .build()
 
-    private val stopScanCallback = object : ScanCallback() {
-        @SuppressLint("MissingPermission")
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            with(result.device) {
-                Log.i("StopScanCallback", "Scan stopped.")
-            }
-        }
-    }
-
     private val scanCallback = object : ScanCallback() {
         @SuppressLint("MissingPermission")
         override fun onScanResult(callbackType: Int, result: ScanResult) {
             with(result.device) {
                 Log.i("ScanCallback", "Found BLE device! Name: ${name ?: "Unnamed"}, address: $address")
-                bleScanner.stopScan(stopScanCallback)
             }
         }
     }
